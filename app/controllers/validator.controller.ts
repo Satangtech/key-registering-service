1abc41d0eee40522a9ae5f9c437fe5a6e280c317
Ic3Sandy <monkey_king2@hotmail.com>
2022-09-13 18:15:00 +0700

    🚧 Update api connect to db

--- app/controllers/validator.controller.ts
+++ app/controllers/validator.controller.ts
@@ -1,24 +1,43 @@
+import { wrap } from "@mikro-orm/core";
 import { Request, Response } from "express";
 import Router from "express-promise-router";
 import { Status } from "../entities/Validator";
+import { DI } from "../server";
 
 const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
-  const { id, publickey } = req.body;
-  return res.send({
-    id,
-    publickey,
-    status: Status.Pending,
-  });
+  if (!req.body.id || !req.body.publickey) {
+    res.status(400);
+    return res.json({ message: "One of `id, publickey` is missing" });
+  }
+  try {
+    req.body.validatorId = req.body.id;
+    delete req.body.id;
+    const validator = DI.validatorRepository.create(req.body);
+    await DI.validatorRepository.persist(validator).flush();
+
+    return res.json({
+      id: validator.validatorId,
+      publickey: validator.publickey,
+      status: validator.status,
+    });
+  } catch (e: any) {
+    return res.status(400).json({ message: e.message });
+  }
 });
 
 router.put("/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+  const { id: validatorId } = req.params;
   const { publickey, status } = req.body;
-  return res.send({
-    publickey,
-    status: "updating",
+  const validator = await DI.validatorRepository.findOneOrFail({ validatorId });
+  wrap(validator).assign({ status: Status.Updating });
+  await DI.validatorRepository.flush();
+
+  return res.json({
+    id: validator.validatorId,
+    publickey: validator.publickey,
+    status: validator.status,
   });
 });
 
--- app/controllers/validators.controller.ts
+++ app/controllers/validators.controller.ts
@@ -1,26 +1,38 @@
 import { Request, Response } from "express";
 import Router from "express-promise-router";
-import { Status } from "../entities/Validator";
+import { DI } from "../server";
+import { QueryOrder } from "@mikro-orm/core";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  return res.send([
-    {
-      id: "validator1",
-      publickey: "publickey1",
-      status: `${Status.Pending}|${Status.Registered}|${Status.Unregistered}|${Status.Updating}`,
-    },
-  ]);
+  const validators = await DI.validatorRepository.findAll({
+    orderBy: { updatedAt: QueryOrder.DESC },
+    fields: ["validatorId", "publickey", "status"],
+  });
+  return res.json(
+    validators.map(({ validatorId, publickey, status }) => ({
+      id: validatorId,
+      publickey,
+      status,
+    }))
+  );
 });
 
 router.get("/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  return res.send({
-    id,
-    publickey: "publickey1",
-    status: `${Status.Pending}|${Status.Registered}|${Status.Unregistered}|${Status.Updating}`,
-  });
+  const { id: validatorId } = req.params;
+  try {
+    const validator = await DI.validatorRepository.findOneOrFail({
+      validatorId,
+    });
+    return res.json({
+      id: validator.validatorId,
+      publickey: validator.publickey,
+      status: validator.status,
+    });
+  } catch (e: any) {
+    return res.status(400).json({ message: e.message });
+  }
 });
 
 export const ValidatorsController = router;
