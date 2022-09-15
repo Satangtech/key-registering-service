a8c641b46a7727654d354d914b63e26f18dcfc74
Ic3Sandy <monkey_king2@hotmail.com>
2022-09-15 15:29:59 +0700

    🚧 Add func get vote

--- app/utils.ts
+++ app/utils.ts
@@ -1,5 +1,19 @@
 import { Network } from "firovm-sdk";
 
+export const getVote = (status: string) => {
+  // 0 => Abstain
+  // 1 => Vote for
+  // 2 => Vote against
+  switch (status) {
+    case "registered":
+      return 1;
+    case "unregistered":
+      return 2;
+    default:
+      return 0;
+  }
+};
+
 export const getNetwork = (network: string): Network => {
   switch (network) {
     case "regtest":
